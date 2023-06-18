6e3a31da23c939917ec952ea0b0784aa117d99cd
Dired <iddersidien@ymail.com>
2023-06-18 05:55:12 +0200

    serve the build with express.static

--- index.js
+++ index.js
@@ -14,12 +14,9 @@ connect();
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
-app.use(cors({
-    origin: ['http://localhost:3000'],
-    credentials: true,
-    sameSite: 'none'
-}));
+app.use(express.urlencoded({extended:true}));
+
+app.use(express.static("build"));
 
 app.use(authRoutes);
 app.use(routes);
